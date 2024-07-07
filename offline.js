68bf1af64ada745fe5927949c899356a6dd0b2cc
gameedukasi <gammeedukasi@gmail.com>
2024-07-07 23:24:26 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1720365523,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/turutan-sheet0.png",
+		"images/papansoal-sheet0.png",
+		"images/bgame-sheet0.png",
+		"images/twaktu-sheet0.png",
+		"images/tnyawa-sheet0.png",
+		"images/tscore-sheet0.png",
+		"images/bjawaban-sheet0.png",
+		"images/nyawa2.png",
+		"images/nyawa1.png",
+		"images/bscore-sheet0.png",
+		"images/close-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
